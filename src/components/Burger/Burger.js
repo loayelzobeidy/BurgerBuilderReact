1060468b5d5e8838b49e7eb8c0af633c17e7690c
Ubuntu18 <loay.el_zobaidy@emc.com>
2018-07-22 17:16:20 -0700

    addding ingrediantscomponent added

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -2,11 +2,21 @@ import React from 'react';
 import classes from './Burger.css'
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 const burger = (props)=>{
+    let transforIngrediants = Object.keys(props.ingrediants).map(igkey=>{
+     return [...Array(props.ingrediants[igkey])].map((_,i)=>{
+   return  <BurgerIngredient type = {igkey}/>
+
+     });
+    }).reduce((arr,el)=>{
+        console.log(arr)
+       return  arr.concat(el);
+    },[]);
+    if(transforIngrediants.length===0)
+    transforIngrediants = <p>pleasee addd ingrediant</p>
 return(
     <div className={classes.Burger}>
     <BurgerIngredient type = "bread-top"/>
-    <BurgerIngredient type = "cheese"/>
-    <BurgerIngredient type = "meat"/>
+{transforIngrediants}
     <BurgerIngredient type = "bread-bottom"/>
     
     </div>
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,14 +1,55 @@
 import React, {Component}from 'react';
 import Aux from '../../hoc/Aux' ;
 import Burger from '../../components/Burger/Burger'
+import BuildControls from '../../components/Burger/BuildControls/BuildControls'
+const INGREDIANT_PRICES = {
+ salad :4,
+}
 class BurgerBuilder extends Component
 {
+    state={
+   ingrediant:{
+       salad:0,
+       bacon:0,
+       meat:0,
+       cheese:0
+   },
+   totalPrice : 4
+    }
+    addIngrediantHandler = (type)=>{
+        const oldCount = this.state.ingrediant[type];
+        const newCount = this.state.ingrediant[type]+1;
+        const updatedIngrediant={
+            ...this.state.ingrediant
+        };
+        updatedIngrediant[type] = newCount;
+        
+        const priceAddition = INGREDIANT_PRICES[type];
+        const oldPrice = this.state.totalPrice;
+        const newPrice = oldPrice+priceAddition;
+        this.setState({ingrediant:updatedIngrediant,totalPrice:newPrice})
+    }
+    removeIngrediantHandler = (type)=>{
+        const oldCount = this.state.ingrediant[type];
+        if(oldCount==0)
+        return
+        const newCount = this.state.ingrediant[type]-1;
+        const updatedIngrediant={
+            ...this.state.ingrediant
+        };
+        updatedIngrediant[type] = newCount;
+        
+        const priceAddition = INGREDIANT_PRICES[type];
+        const oldPrice = this.state.totalPrice;
+        const newPrice = oldPrice-priceAddition;
+        this.setState({ingrediant:updatedIngrediant,totalPrice:newPrice})
+    }
     render(){
     return (
         <Aux>
-            <Burger/>
-            <div>Build Control </div>
+            <Burger ingrediants = {this.state.ingrediant}/>
             
+            <BuildControls  ingrediantAdded = {this.addIngrediantHandler} removeingrediantRemoved={this.removeIngrediantHandler}/>
         </Aux>
     );    
     }
